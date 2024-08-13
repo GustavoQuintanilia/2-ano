bfcd145c94f50ccd298584f283897ce01886528e
Quintanilia <145074850+GustavoQuintanilia@users.noreply.github.com>
2024-08-12 22:46:08 -0300

    Add files via upload

---
+++ DS1/3°Bimestre/atividade_ClassAbstract/Trapezio.java
@@ -0,0 +1,22 @@
+package atividade1;
+
+public class Trapezio extends FormasGeometricas {
+	private double baseMaior;
+	private double baseMenor;
+	private double altura;
+	
+	//--------------CONSTRUTOR----------------------
+	public Trapezio(double baseMaior, double baseMenor, double altura) {
+		super();
+		this.baseMaior = baseMaior;
+		this.baseMenor = baseMenor;
+		this.altura = altura;
+	}
+	//----------------------------------------------
+
+	@Override 
+	public double calcularArea() {
+		return ((baseMaior + baseMenor)*altura)/2;
+	}
+
+}
