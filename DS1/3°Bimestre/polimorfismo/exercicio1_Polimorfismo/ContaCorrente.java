223669bf4a8e656c04c86d68dc5b9a87d6f0a287
Quintanilia <145074850+GustavoQuintanilia@users.noreply.github.com>
2024-09-09 22:51:32 -0300

    Add files via upload

---
+++ DS1/3°Bimestre/polimorfismo/exercicio1_Polimorfismo/ContaCorrente.java
@@ -0,0 +1,28 @@
+package exercicio1_Polimorfismo;
+
+public class ContaCorrente extends Conta{
+	private double tarifaMensal;
+	
+//------------------------CONSTRUTORES-----------------------------
+	public ContaCorrente() {
+	
+	}
+	
+	public ContaCorrente(double tarifaMensal) {
+		this.tarifaMensal = tarifaMensal;
+	}
+	
+//------------------------ATRIBUTOS-----------------------------		
+//----------------------------------------	
+	public double getTarifaMensal() {
+		return tarifaMensal;
+	}
+	
+
+	public void setTarifaMensal() {
+		this.tarifaMensal = tarifaMensal;
+	}
+	
+	
+
+}
