766f4863a6de90b0a6fdd9465c813103ad8350c2
Quintanilia <145074850+GustavoQuintanilia@users.noreply.github.com>
2024-09-27 07:48:50 -0300

    Add files via upload

---
+++ PAM/3º Bimestre/PAM_27.09/PAM_27.09/calcular.js
@@ -0,0 +1,28 @@
+//Arrumar -Usuário  inclui valores
+var v1 = 5;
+var v2 = 5;
+
+function soma(){
+    res =  v1+v2;
+    console.log(res)
+}
+
+function subtrair(){
+    res = v1-v2;
+    console.log(res)
+}
+
+function multiplicacao(){
+    res =  v1*v2;
+    console.log(res)
+}
+
+function divisao(){
+    res = v1/v2;
+    console.log(res)
+}
+
+soma();
+subtrair();
+multiplicacao();
+divisao();
